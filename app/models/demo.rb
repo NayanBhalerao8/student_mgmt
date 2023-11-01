877ff9141a0c5747ab5695a03144299b08631744
Nayan Bhalerao <rubydev.nayan@gmail.com>
2023-11-01 22:38:19 +0530

    active record callback

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -1,3 +1,14 @@
 class Demo < ApplicationRecord
     has_one :sub_demo
+
+    before_create :greeting_message 
+
+    def greeting_message 
+        puts "Hey i will run before you create an object "
+    end
+
+    # or you can write as this way 
+    # before_create  do 
+    #     puts "second meee"
+    # end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,14 @@ class Student < ApplicationRecord
     has_and_belongs_to_many :courses
     has_many :student_projects
     has_many :projects, through: :student_projects
+    after_commit :display_student_age 
+
+    def display_student_age
+        if self.date_of_birth.present? 
+            age = Date.today.year - date_of_birth.year
+            puts "age of the student is #{age}"
+        else 
+            puts "======age cannot be calculated=========="
+        end
+    end
 end
