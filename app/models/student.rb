6449c9834ae6d5bf820149292a7c0d434d32425c
Nayan Bhalerao <rubydev.nayan@gmail.com>
2023-11-01 23:00:50 +0530

    custom validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,7 +4,15 @@ class Student < ApplicationRecord
     validates :email, uniqueness: true
     validates :first_name, :last_name, length: {minimum:10, maximum:50}
     validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\Z/, message: "only letters are allowed"}
+    # validates :age, presence: true, numericality: true
+    # this check if the value is number 
     
+    # custome validation 
+    validate :validate_student_age 
+
+
+
+
     has_and_belongs_to_many :courses
     has_many :student_projects
     has_many :projects, through: :student_projects
@@ -16,6 +24,15 @@ class Student < ApplicationRecord
             puts "age of the student is #{age}"
         else 
             puts "======age cannot be calculated=========="
+        end
+    end
+
+    def validate_student_age
+        if self.date_of_birth.present?
+            age = Date.today.year - self.date_of_birth.year
+            if age < 15
+                errors.add(:age, "please provide valid date of birth, age should be greater than 15")
+            end
         end
     end
 end
