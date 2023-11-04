592ee7552f102d92e87fbc51d698460b41201546
Nayan Bhalerao <rubydev.nayan@gmail.com>
2023-11-04 15:57:53 +0530

    helper and actice record concerns

---
+++ app/controllers/admin/students_controller.rb
@@ -0,0 +1,3 @@
+class Admin::StudentController < ApplicationController 
+    # student controller
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,35 @@ Rails.application.routes.draw do
   resources :courses
   root 'welcome#index'
 
-  resources :students
+  resources :students do 
+     member do  
+      get :personal_details 
+     end 
+
+    #  another form
+    #  get :personal_details  on: :member
+
+     collection do 
+        get :activity
+     end
+
+    #  another form
+    #  get :activity  on: :collection
+  end
+
+  namespace :admin do 
+    resources :students
+  end
+
+
+  # scope module: :admin do 
+  #   resources :students
+  # end
+
+  get 'welcome/about'
+  get 'about', to: 'welcome#about'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
